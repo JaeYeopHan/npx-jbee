70199c0ecf020467fba292b06ed32adba4d7527a
JaeYeopHan <ljyhanll@gmail.com>
2020-07-12 01:06:50 +0900

    feat: add component

--- src/jbee.tsx
+++ src/jbee.tsx
@@ -1,6 +1,46 @@
 import React from "react";
-import { Text } from "ink";
+import { Color, Box, Text } from "ink";
 
 export const Jbee = () => {
-  return <Text>Hello, I'm Jbee</Text>;
+  return (
+    <Box margin={1} padding={2} flexDirection="column">
+      <Box marginBottom={1} alignItems="center">
+        <Color cyan>Hello, </Color>
+        <Color cyanBright>I'm </Color>
+        <Color bgBlue white>
+          <Text bold> Jbee </Text>
+        </Color>
+        <Text> 🐝</Text>
+      </Box>
+      <Box marginBottom={1}>
+        <Color yellow>Front-End Engineer</Color>
+        <Color magenta> or </Color>
+        <Color green>Product Developer</Color>
+      </Box>
+      <Box flexDirection="column" marginBottom={1}>
+        <Text>
+          <Color bgWhiteBright black>
+            {"  "}
+            GitHub{" "}
+          </Color>
+          <Text underline> https://github.com/JaeYeopHan</Text>
+        </Text>
+        <Text>
+          <Color bgWhiteBright black>
+            {"    "}
+            Blog{" "}
+          </Color>
+          <Text underline> https://jbee.io</Text>
+        </Text>
+        <Text>
+          <Color bgWhiteBright black>
+            {" "}
+            Twitter{" "}
+          </Color>
+          <Text underline> https://twitter.com/JbeeLjyhanll</Text>
+        </Text>
+      </Box>
+      <Color gray>Out of box, pay it forward 👋</Color>
+    </Box>
+  );
 };
